5f6501e68851a87f210d6be9410fc1b31210940e
William Moore <wmoore98@sbcglobal.net>
2021-02-04 14:07:03 -0500

    Organize actions and action types

---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,26 @@
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+
+interface RepositoriesState {
+  isLoading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+const reducer = (
+  state: RepositoriesState,
+  action: Action
+): RepositoriesState => {
+  switch (action.type) {
+    case ActionType.SEARCH_REPOSITORIES:
+      return { isLoading: true, error: null, data: [] };
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+      return { isLoading: false, error: null, data: [...action.payload] };
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
+      return { isLoading: false, error: action.payload, data: [] };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
