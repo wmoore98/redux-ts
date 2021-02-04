a4a3d172e77619ca676accad31f8a8154828a80a
William Moore <wmoore98@sbcglobal.net>
2021-02-04 18:17:29 -0500

    Render results

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,13 +1,29 @@
 import { useState } from "react";
 import { useActions } from "../hooks/useActions";
+import { usedTypedSelector } from "../hooks/useTypedSelector";
 
 export const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState("");
   const { searchRepositories } = useActions();
+  const { data, isLoading, error } = usedTypedSelector(
+    (state) => state.repositories
+  );
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
     searchRepositories(term);
+  };
+
+  const renderData = () => {
+    return data.length ? (
+      <ul>
+        {data.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+    ) : (
+      <div>No packages found</div>
+    );
   };
 
   return (
@@ -20,7 +36,10 @@ export const RepositoriesList: React.FC = () => {
         />
         <button>Search</button>
       </form>
-      <p>{term}</p>
+      <hr />
+      {error && <h3>{error}</h3>}
+      {isLoading && <h3>Loading...</h3>}
+      {!error && !isLoading && renderData()}
     </div>
   );
 };
